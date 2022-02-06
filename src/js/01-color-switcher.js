2fbf89643d6b88f903818efc868db3a36dc30eab
Yulia Klimchuk <yulia.melnichenko.995@gmail.com>
2022-02-06 12:05:08 +0200

    add task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
 
+btnStart.addEventListener('click', handleStartClick);
+btnStop.addEventListener('click', handleStopClick);
+
+function handleStartClick() { 
+    timerId = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+    btnStart.setAttribute('disabled', true);
+    btnStop.removeAttribute('disabled');
+};
+
+function handleStopClick() { 
+    clearInterval(timerId);
+    btnStart.removeAttribute('disabled');
+    btnStop.setAttribute('disabled', true);
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
